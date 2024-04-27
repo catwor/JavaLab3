dc91bc9011e9d7fba0855b5eb898d84c8c308574
Ivan Tetyukhin <itetyukhin@digital-spirit.ru>
2024-04-27 16:00:17 +0400

    add java doc

--- src/main/java/org/example/TimerForListFunctions.java
+++ src/main/java/org/example/TimerForListFunctions.java
@@ -1,81 +1,104 @@
 package org.example;
 
-import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.function.Consumer;
-import java.util.function.Predicate;
 
+/**
+ * Класс TimerForListFunctions предоставляет методы для измерения времени выполнения операций над списками.
+ */
 public class TimerForListFunctions {
 
-  public static double[] timeAdd(){
+  /**
+   * Метод для измерения времени выполнения операции добавления элементов в список.
+   * @return массив, содержащий время выполнения операции добавления для ArrayList и LinkedList в миллисекундах.
+   */
+  public static double[] timeAdd() {
     List<Integer> array = new ArrayList<>();
     List<Integer> linked = new LinkedList<>();
 
-    double resultArrayList = timer((list)->{
-      fillList(list,5000);
+    double resultArrayList = timer((list) -> {
+      fillList(list, 5000);
     }, array);
 
-    double resultLinkedList = timer((list)->{
-      fillList(list,5000);
+    double resultLinkedList = timer((list) -> {
+      fillList(list, 5000);
     }, linked);
 
-    return new double[] {resultArrayList, resultLinkedList};
+    return new double[]{resultArrayList, resultLinkedList};
   }
 
-  public static double[] timeGet(){
+  /**
+   * Метод для измерения времени выполнения операции получения элементов из списка.
+   * @return массив, содержащий время выполнения операции получения для ArrayList и LinkedList в миллисекундах.
+   */
+  public static double[] timeGet() {
     List<Integer> array = new ArrayList<>();
     List<Integer> linked = new LinkedList<>();
 
-    fillList(array,5000);
-    fillList(linked,5000);
+    fillList(array, 5000);
+    fillList(linked, 5000);
 
-    double resultArrayList = timer((list)->{
-      for(int i=0;i<list.size();++i)
+    double resultArrayList = timer((list) -> {
+      for (int i = 0; i < list.size(); ++i)
         list.get(i);
     }, array);
 
-    double resultLinkedList = timer((list)->{
-      for(int i=0;i<list.size();++i)
+    double resultLinkedList = timer((list) -> {
+      for (int i = 0; i < list.size(); ++i)
         list.get(i);
     }, linked);
 
-    return new double[] {resultArrayList, resultLinkedList};
+    return new double[]{resultArrayList, resultLinkedList};
   }
 
-  public static double[] timeRemove(){
+  /**
+   * Метод для измерения времени выполнения операции удаления элементов из списка.
+   * @return массив, содержащий время выполнения операции удаления для ArrayList и LinkedList в миллисекундах.
+   */
+  public static double[] timeRemove() {
     List<Integer> array = new ArrayList<>();
     List<Integer> linked = new LinkedList<>();
 
-    fillList(array,5000);
-    fillList(linked,5000);
+    fillList(array, 5000);
+    fillList(linked, 5000);
 
-    double resultArrayList = timer((list)->{
-      for(int i=0;i<list.size();++i)
+    double resultArrayList = timer((list) -> {
+      for (int i = 0; i < list.size(); ++i)
         list.remove(i);
     }, array);
 
-    double resultLinkedList = timer((list)->{
-      for(int i=0;i<list.size();++i)
+    double resultLinkedList = timer((list) -> {
+      for (int i = 0; i < list.size(); ++i)
         list.remove(i);
     }, linked);
 
-    return new double[] {resultArrayList, resultLinkedList};
+    return new double[]{resultArrayList, resultLinkedList};
   }
 
+  /**
+   * Заполняет список указанным количеством элементов.
+   * @param list  список, который нужно заполнить
+   * @param count количество элементов, которыми нужно заполнить список
+   */
   private static void fillList(List<Integer> list, int count) {
     for (int i = 0; i < count; ++i)
       list.add(i);
   }
 
-  private static double timer(Consumer<List> fun, List list){
+  /**
+   * Измеряет время выполнения операции над списком.
+   * @param fun  операция, которую нужно измерить
+   * @param list список, над которым выполняется операция
+   * @return время выполнения операции в миллисекундах
+   */
+  private static double timer(Consumer<List> fun, List list) {
     long start = System.nanoTime();
 
     fun.accept(list);
 
     long end = System.nanoTime();
-    return (double) ((end - start)/1_000_000.0);
+    return (double) ((end - start) / 1_000_000.0);
   }
 }
